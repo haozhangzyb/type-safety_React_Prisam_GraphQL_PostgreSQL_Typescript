0804938e5cd995154c4f5f320e2bb7cc86208ea0
Hao Zhang <haozhangzyb@outlook.com>
2023-05-04 15:11:40 -0400

    init project with Vite and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='bg-black h-screen flex flex-col justify-center items-center'>
+      <p className='font-extrabold text-red-300 text-4xl'>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
