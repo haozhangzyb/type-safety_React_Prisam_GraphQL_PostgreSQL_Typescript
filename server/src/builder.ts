37115be3fd7959fda704c80192bc28545bfd1ec3
Hao Zhang <haozhangzyb@outlook.com>
2023-05-05 13:51:09 -0400

    Add the Pothos Prisma plugin

--- server/src/builder.ts
+++ server/src/builder.ts
@@ -1,10 +1,12 @@
 import SchemaBuilder from "@pothos/core";
 import { DateResolver } from "graphql-scalars";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
 
 export const builder = new SchemaBuilder<{
   Scalars: {
     Date: { Input: Date; Output: Date };
   };
-}>({});
+}>({ plugins: [PrismaPlugin] });
 
 builder.addScalarType("Date", DateResolver, {});
