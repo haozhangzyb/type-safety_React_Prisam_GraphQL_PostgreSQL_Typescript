4cf788ce898b46534cbebb0fb254c087d36cd1c1
Hao Zhang <haozhangzyb@outlook.com>
2023-05-05 14:25:34 -0400

    implement queries

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,4 +1,5 @@
 import { builder } from "../builder";
+import { prisma } from "../db";
 
 builder.prismaObject("User", {
   fields: (t) => ({
@@ -7,3 +8,24 @@ builder.prismaObject("User", {
     messages: t.relation("messages"),
   }),
 });
+// type User {
+//     id: ID!
+//     messages: [Message!]!
+//     name: String!
+//   }
+
+// Adds a field to the GraphQL schema's Query type named "users"
+// Defines a field that resolves to some type in your Prisma schema
+// Lets Pothos know this field will resolve to an array of your Prisma Client's User type
+// Sets up a resolver function for this field.
+builder.queryField("users", (t) =>
+  t.prismaField({
+    type: ["User"],
+    resolve: async (query, root, args, ctx, info) =>
+      prisma.user.findMany({ ...query }),
+  })
+);
+
+// type Query {
+//     users: [User!]!
+//   }
